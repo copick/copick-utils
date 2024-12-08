9ae3fc578d357b0784ec312b5c46da8f7c657aa5
Kyle Harrington <czi@kyleharrington.com>
2024-12-08 14:29:12 -0500

    Re-add doc strings

--- src/copick_utils/segmentation/segmentation_from_picks.py
+++ src/copick_utils/segmentation/segmentation_from_picks.py
@@ -3,10 +3,30 @@ import zarr
 from scipy.ndimage import zoom
 import copick
 
-
-def from_picks(pick, seg_volume, radius=10.0, label_value=1, voxel_spacing=10):
+def from_picks(pick,
+               seg_volume,
+               radius: float = 10.0,
+               label_value: int = 1,
+               voxel_spacing: float = 10):
     """
     Paints picks into a segmentation volume as spheres.
+
+    Parameters:
+    -----------
+    pick : copick.models.CopickPicks
+        Copick object containing `points`, where each point has a `location` attribute with `x`, `y`, `z` coordinates.
+    seg_volume : numpy.ndarray
+        3D segmentation volume (numpy array) where the spheres are painted. Shape should be (Z, Y, X).
+    radius : float, optional
+        The radius of the spheres to be inserted in physical units (not voxel units). Default is 10.0.
+    label_value : int, optional
+        The integer value used to label the sphere regions in the segmentation volume. Default is 1.
+    voxel_spacing : float, optional
+        The spacing of voxels in the segmentation volume, used to scale the radius of the spheres. Default is 10.
+    Returns:
+    --------
+    numpy.ndarray
+        The modified segmentation volume with spheres inserted at pick locations.    
     """
     def create_sphere(shape, center, radius, val):
         zc, yc, xc = center
@@ -61,7 +81,33 @@ def downsample_to_exact_shape(array, target_shape):
 
 def segmentation_from_picks(radius, painting_segmentation_name, run, voxel_spacing, tomo_type, pickable_object, pick_set, user_id="paintedPicks", session_id="0"):
     """
-    Paints picks into the highest resolution scale and generates lower scales by downsampling.
+    Paints picks from a run into a multiscale segmentation array, representing them as spheres in 3D space.
+
+    Parameters:
+    -----------
+    radius : float
+        Radius of the spheres in physical units.
+    painting_segmentation_name : str
+        The name of the segmentation dataset to be created or modified.
+    run : copick.Run
+        The current Copick run object.
+    voxel_spacing : float
+        The spacing of the voxels in the tomogram data.
+    tomo_type : str
+        The type of tomogram to retrieve.
+    pickable_object : copick.models.CopickObject
+        The object that defines the label value to be used in segmentation.
+    pick_set : copick.models.CopickPicks
+        The set of picks containing the locations to paint spheres.
+    user_id : str, optional
+        The ID of the user creating the segmentation. Default is "paintedPicks".
+    session_id : str, optional
+        The session ID for this segmentation. Default is "0".
+
+    Returns:
+    --------
+    copick.Segmentation
+        The created or modified segmentation object.
     """
     # Fetch the tomogram and determine its multiscale structure
     tomogram = run.get_voxel_spacing(voxel_spacing).get_tomogram(tomo_type)
